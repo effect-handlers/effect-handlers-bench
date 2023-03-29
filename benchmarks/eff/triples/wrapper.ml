1613b1b316aecfff00421f95017bab3cd7cdd541
Philipp Schuster <philipp.schuster@uni-tuebingen.de>
2023-03-29 17:25:50 +0200

    Synchronize triples benchmark

--- benchmarks/eff/triples/wrapper.ml
+++ benchmarks/eff/triples/wrapper.ml
@@ -1,5 +1,3 @@
 let n = try int_of_string Sys.argv.(1) with _ -> 300
 
-let s = try int_of_string Sys.argv.(2) with _ -> 300
-
-let _ = Printf.printf "%d\n" (Generated.sum_triples n s)
+let _ = Printf.printf "%d\n" (Generated.run n n)
--- benchmarks/ocaml/triples/main.ml
+++ benchmarks/ocaml/triples/main.ml
@@ -1,9 +1,3 @@
-effect Choose : unit -> bool
-
-let s = try int_of_string (Sys.argv.(1)) with _ -> 300
-
-let n = try int_of_string (Sys.argv.(2)) with _ -> 300
-
 
 effect Flip : unit -> bool
 exception Fail
@@ -20,27 +14,29 @@ let triple n s =
   let j = choice (i - 1) in
   let k = choice (j - 1) in
   if (i + j + k = s)
-    then ((i, j, k) )
+    then (i, j, k)
     else raise Fail
 
-let hash_triple (a,b,c) = 
+let hash (a,b,c) =
   (53 * a + 2809 * b + 148877 * c) mod 1000000007 (* 2089 = 53 * 53, 148877 = 53 * 2089  *)
 
-let main n s =
-  let sum = 
-    try
-    try
-      let r = triple n s in
-      hash_triple r
-    with
-    | Fail -> 0
-    with
-    | effect (Flip ()) k ->
-        let l = continue (Obj.clone_continuation k) true in
-        let r = continue k false in
-        (l + r) mod 1000000007
-  in
-  Printf.printf "%d\n" sum
+let run n s =
+  try
+  try
+    hash (triple n s)
+  with
+  | Fail ->
+     0
+  with
+  | effect (Flip ()) k ->
+    let l = continue (Obj.clone_continuation k) true in
+    let r = continue k false in
+    (l + r) mod 1000000007
 
-let _ = main s n
+let main () =
+  let n = try int_of_string (Sys.argv.(1)) with _ -> 10 in
+  let r = run n n in
+  Printf.printf "%d\n" r
+
+let _ = main ()
 
