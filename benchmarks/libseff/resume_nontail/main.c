b197bdb97d139b95a6be691eb20237e952c7f50d
codrutiftode <codriniftode@gmail.com>
2024-05-27 16:23:18 +0100

    Fix resume_nontail

--- benchmarks/libseff/resume_nontail/main.c
+++ benchmarks/libseff/resume_nontail/main.c
@@ -2,53 +2,51 @@
 #include <math.h>
 #include <stdio.h>
 
-DEFINE_EFFECT(op, 0, void, { int32_t x; });
+DEFINE_EFFECT(op, 0, void, { int64_t x; });
 
-inline int32_t binaryOperation(int32_t x, int32_t y) { 
+inline int64_t binaryOperation(int64_t x, int64_t y) { 
   return (labs(x - (503 * y) + 37)) % 1009;
 }
 
-seff_request_t req;
-
-int32_t handleOp(seff_coroutine_t *k) {
-  req = seff_handle(k, NULL, HANDLES(op));
+int64_t handleOpRec(seff_coroutine_t *k) {
+  seff_request_t req = seff_handle(k, NULL, HANDLES(op));
   switch (req.effect) {
     CASE_EFFECT(req, op, {
-      return binaryOperation(payload.x, handleOp(k));
+      return binaryOperation(payload.x, handleOpRec(k));
     })
     CASE_RETURN(req, {
-      return (int32_t) payload.result;
+      return (int64_t) payload.result;
     })
   };
   return -1;
 }
 
 typedef struct loop_args_t {
-  int32_t n;
-  int32_t s;
+  int64_t n;
+  int64_t s;
 } loop_args_t;
 
 void* loop(void* parameter) {
-  loop_args_t* args = parameter; 
+  loop_args_t* args = (loop_args_t*) parameter;
   if (args->n == 0) return (void*) args->s;
-  PERFORM (op, args->n);
+  PERFORM(op, args->n);
   args->n--;
-  return loop(args);
+  return loop((void*) args);
 }
 
-int32_t run(int32_t n, int32_t s) {
+int64_t run(int64_t n, int64_t s) {
   loop_args_t args = {
     .n = n,
     .s = s
   };
-  seff_coroutine_t *k = seff_coroutine_new(loop, (void*) &args);
-  int32_t result = handleOp(k);
+  seff_coroutine_t *k = seff_coroutine_new(loop, &args);
+  int64_t result = handleOpRec(k);
   seff_coroutine_delete(k);
   return result;
 }
 
-int32_t repeat(int32_t n) {
-  int32_t s;
+int64_t repeat(int64_t n) {
+  int64_t s = 0;
   for (int i = 1000; i > 0; i--) {
     s = run(n, s);
   }
@@ -56,8 +54,8 @@ int32_t repeat(int32_t n) {
 }
 
 int main(int argc, char** argv) { 
-  int32_t n = argc != 2 ? 5 : atoi(argv[1]);
-  int32_t r = repeat(n);
-  printf("%d\n", r);
+  int64_t n = argc != 2 ? 5 : atoi(argv[1]);
+  int64_t r = repeat(n);
+  printf("%ld\n", r);
   return 0; 
 }
